5ca83545df0577bb29e15657e838b8ed35ce0104
Rigoberto Alcantar Aguilar <a20150338@utem.edu.mx>
2018-12-06 16:03:38 -0600

    Operators
    Basic examples about operators

---
+++ operadores/operadores.js
@@ -0,0 +1,9 @@
+var suma = 5 + 5
+var resta = 1024 - 45
+var division = 100 / 2
+var multiplicacion = 180 * 500
+
+console.log("La suma es " + suma, 
+            "La resta es " + resta, 
+            "La division es " + division, 
+            "La multiplicacion es " + multiplicacion)
